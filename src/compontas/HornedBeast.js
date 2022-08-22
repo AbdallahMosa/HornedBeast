1384c0cfc1b970b8baa8cd4319e5dab173500a0d
AbdallahMosa <abdallahmosa95@yahoo.com>
2022-08-22 19:42:35 +0300

    70%

--- src/compontas/HornedBeast.js
+++ src/compontas/HornedBeast.js
@@ -1,16 +1,46 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
-render(){
-    return(<div>
-                <h2>{this.props.tital}</h2>
-                <img src={this.props.imageUrl} alt="Cat"></img>
-                <p>{this.props.description}</p>
+    constructor(props) {
+        super(props);
+        this.state = {
+            Number : 0
+        }
+    }
+
+    incremantNum = () => {
+        this.setState({
+            Number : this.state.Number + 1
+        })
+    }
 
 
 
-    </div>)
-}
+render(){return(
+    // <div>
+    //             <h2>{this.props.tital}</h2>
+    //             <img src={this.props.imageUrl} alt="Cat"></img>
+    //             <p>{this.props.description}</p>
+
+
+
+    // </div>
+     <Card style={{ width: '18rem' }} class="card">
+     <Card.Img variant="top" src={this.props.Img} onClick={this.incremantNum}/>
+     <Card.Body>
+       <Card.Title>{this.props.tital}</Card.Title>
+       <Card.Text>
+       {this.props.Des}
+       <div>Number : {this.state.Number}</div>
+       <div>number of horns: {this.props.Horne}</div>
+       <div>The name : {this.props.Name}</div>
+       </Card.Text>
+       
+     </Card.Body>
+   </Card>
+)}
 
 }
 export default HornedBeast
--- src/compontas/Main.js
+++ src/compontas/Main.js
@@ -1,12 +1,31 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import Cat from './assets/cat.jpg'
-class Main extends React.Component{
-    render(){
-        return(<div>
-                <HornedBeast tital='Cat' imageUrl={Cat} description ='a pet and cute animal '/>
-              
-        </div>)
+// import Cat from './assets/cat.jpg'
+import JSON from "./Data.json";
+
+  const allData =JSON.map(element =>
+        <HornedBeast  
+            Title = {element.title}
+            ID = {element._id}
+            Img = {element.image_url}
+            Des = {element.description}
+            Horne = {element.horns}
+            Name ={element.keyword}
+        /> 
+  )
+  
+
+        class Main extends React.Component{
+
+   render(){
+        return(allData )
+            // <div>
+        //         {/* <HornedBeast tital='Cat' imageUrl={Cat} description ='a pet and cute animal '/> */}       
+        /* // </div> */
+        
     }
-}
-export default Main 
+
+        
+    };
+   
+export default Main ;
